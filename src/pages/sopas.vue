da0561b7442502dc6b696c18cb55b339033e8cc3
Ph4ra0hXX <ph4ra0hx@gmail.com>
2025-04-15 23:04:13 -0300

    teste

---
+++ src/pages/sopas.vue
@@ -0,0 +1,230 @@
+<script>
+import { ref, watch, onUpdated } from "vue";
+import { carrinhoStore } from "../store/produtos";
+import { useToast } from "vue-toastification";
+import router from "../router/index";
+
+export default {
+  setup() {
+    const toast = useToast();
+
+    const carrinho = carrinhoStore();
+
+    const Burger = ref({
+      Pao: [
+        {
+          nome: "Burguer Drácula - CARNE",
+          preco: 18.0,
+          quantidade: 0,
+          descricao:
+            "Pão brioche black selado na manteiga, hambúrguer caseiro, baba de bruxa, bacon, ovo, tomate, alface e cebola.",
+        },
+        {
+          nome: "Burguer Coringa - FRANGO",
+          preco: 15.0,
+          quantidade: 0,
+          descricao:
+            "Pão brioche red selado na manteiga, filé de frango trinchado, sangue de bruxa, bacon, ovo, tomate, alface e cebola.",
+        },
+      ],
+      Adicionais: [
+        { nome: "Filé de Frango Trinchado", preco: 5.0, quantidade: 0 },
+        { nome: "Calabresa", preco: 3.0, quantidade: 0 },
+        { nome: "Boi Trinchado", preco: 8.0, quantidade: 0 },
+        { nome: "Presunto Peru", preco: 2.5, quantidade: 0 },
+        { nome: "Carne de Sol Desfiada", preco: 5.0, quantidade: 0 },
+        { nome: "Batata Palha", preco: 1.5, quantidade: 0 },
+        { nome: "Queijo Mussarela", preco: 2.5, quantidade: 0 },
+        { nome: "Milho Verde", preco: 1.0, quantidade: 0 },
+        { nome: "Queijo Cheddar", preco: 3.0, quantidade: 0 },
+        { nome: "Queijo Coalho", preco: 2.0, quantidade: 0 },
+        { nome: "Bacon", preco: 3.0, quantidade: 0 },
+      ],
+    });
+
+    function salvarPedido() {
+      console.log(Burger.value);
+
+      carrinho.burgers.push(Burger.value);
+
+      router.push("/");
+
+      toast.success("Adicionado com sucesso!", {
+        timeout: 2000,
+        position: "top-right",
+        icon: false,
+        showCloseButtonOnHover: true,
+      });
+    }
+
+    function voltar() {
+      router.push("/");
+    }
+
+    return {
+      Burger,
+      salvarPedido,
+      voltar,
+    };
+  },
+};
+</script>
+
+<template>
+  <div id="cardapio">
+    <div id="listar">
+      <div class="dotted-line">
+        <hr />
+        <span id="textDividers">especial de halloween</span>
+        <hr />
+      </div>
+      <div v-for="(item, index) in Burger.Pao" :key="item">
+        <button class="botao1" @click="item.quantidade++">+</button>
+
+        <button
+          v-if="item.quantidade > 0"
+          class="botao2"
+          @click="item.quantidade--"
+        >
+          -
+        </button>
+
+        <label style="pointer-events: none" id="nomeItem" for="adicional"
+          ><span id="quantidadeDiv">{{ item.quantidade }}x</span>
+          {{ item.nome }}</label
+        >
+        <label id="preco">R$: {{ item.preco.toFixed(2) }}</label>
+        <p id="itens">{{ item.descricao }}</p>
+        <br />
+      </div>
+      <!--     <div class="dotted-line">
+        <hr />
+        <span id="textDividers">Adicionais</span>
+        <hr />
+      </div>
+      <div v-for="(item, index) in Burger.Adicionais" :key="item">
+        <button class="botao1" @click="item.quantidade++">+</button>
+
+        <button
+          v-if="item.quantidade > 0"
+          class="botao2"
+          @click="item.quantidade--"
+        >
+          -
+        </button>
+
+        <label style="pointer-events: none" id="nomeItem" for="adicional"
+          ><span id="quantidadeDiv">{{ item.quantidade }}x</span>
+          {{ item.nome }}</label
+        >
+        <label id="preco">R$: {{ item.preco.toFixed(2) }}</label>
+        <p id="itens"></p>
+        <br />
+      </div>
+-->
+      <!---------------------------->
+
+      <button @click="salvarPedido" id="butOpcoes" type="submit" value="Submit">
+        adicionar
+      </button>
+
+      <button id="butOpcoes2" @click="voltar()" type="submit" value="Submit">
+        voltar
+      </button>
+    </div>
+  </div>
+</template>
+
+<style>
+#observacao {
+  padding: 5px;
+  margin-bottom: 20px;
+  height: 60px;
+}
+
+#quantidadeDiv {
+  font-weight: bold;
+  color: #ff4f01;
+  font-size: 20px;
+}
+
+#textDividers {
+  color: #ff4f01;
+  font-family: "Harrison-Rough";
+  font-size: 40px;
+  font-weight: lighter;
+  text-transform: uppercase;
+}
+
+.dotted-line {
+  display: flex;
+  align-items: center;
+  text-align: center;
+  margin: 20px 0;
+}
+
+.dotted-line hr {
+  flex: 1;
+  border: none;
+  border-top: 2px dashed white;
+  margin: 0 10px;
+}
+
+.dotted-line span {
+  font-size: 18px;
+  font-weight: bold;
+  color: #333;
+}
+
+.botao1 {
+  background-color: #ff4f01;
+  color: #000000;
+  border: none;
+  padding: 5px 10px;
+  font-size: 20px;
+  font-weight: bold;
+  cursor: pointer;
+  border-radius: 5px;
+  border-bottom-left-radius: 20px;
+  border-top-left-radius: 20px;
+  width: 35px;
+  margin-right: 2px;
+}
+
+.botao2 {
+  background-color: #ff4f01;
+  color: #000000;
+  border: none;
+  padding: 5px 10px;
+  font-size: 20px;
+  font-weight: bold;
+  cursor: pointer;
+  border-radius: 5px;
+  border-bottom-right-radius: 20px;
+  border-top-right-radius: 20px;
+  width: 35px;
+  margin-left: 2px;
+}
+
+#nomeItem {
+  font-size: 16px;
+  margin-left: 7px;
+}
+
+#tituloRadio {
+  display: flex;
+  align-items: center;
+}
+
+#tituloDoRadio {
+  margin-left: 3px;
+}
+
+input[type="radio"] {
+  margin-left: -3px;
+  border: 0px;
+  width: 30px;
+  height: 2em;
+  accent-color: #f27141;
+}
+</style>
---
+++ src/store/produtos.js
@@ -0,0 +1,306 @@
+import { defineStore } from "pinia";
+
+export const carrinhoStore = defineStore("carrinho", {
+  state: () => {
+    return {
+      burgers: [],
+      macarronadas: [],
+      batatas: [],
+      combos: [],
+      bebidas: [],
+      sobremesas: [],
+      dadosPessoais: {
+        nome: "",
+        rua: "",
+        bairro: "",
+        numero: "",
+        referencia: "",
+        formaDeEntrega: "",
+        formaDePagamento: "",
+        troco: "",
+      },
+      pedidos: [],
+      valorTotal: 0,
+      quantidadeDeItens: 0,
+      observacao: "",
+    };
+  },
+  getters: {
+    getValorTotal() {
+      let total = 0;
+
+      var pedidos = [
+        ...this.burgers,
+        ...this.macarronadas,
+        ...this.batatas,
+        ...this.combos,
+        ...this.sobremesas,
+        ...this.bebidas,
+      ];
+
+      pedidos.forEach((categoria) => {
+        for (const chave in categoria) {
+          categoria[chave].forEach((item) => {
+            total += item.preco * item.quantidade;
+          });
+        }
+      });
+
+      return total.toFixed(2);
+    },
+    getTotalPedidos() {
+      return (
+        this.burgers.length +
+        this.macarronadas.length +
+        this.batatas.length +
+        this.combos.length +
+        this.sobremesas.length +
+        this.bebidas.length
+      );
+    },
+  },
+});
+
+export const produtosStore = defineStore("produto", {
+  state: () => {
+    return {
+      cardDataStore: [
+         /* {
+          id: 9,
+          nome: "ASSUSTADORAMENTE",
+          img: "food9.webp",
+          descricao: "Delicioso",
+          url: "/sopas",
+          diasSemana: [
+            "segunda-feira",
+            "terça-feira",
+            "quarta-feira",
+            "quinta-feira",
+            "sexta-feira",
+            "sábado",
+            "domingo",
+          ],
+        },*/
+        {
+          id: 1,
+          nome: "BURGUER",
+          img: "food1.webp",
+          descricao: "Monte do seu jeito",
+          url: "/burger",
+          diasSemana: [
+            "segunda-feira",
+            "terça-feira",
+            "quarta-feira",
+            "quinta-feira",
+            "sexta-feira",
+            "sábado",
+            "domingo",
+          ],
+        },
+        {
+          id: 8,
+          nome: "COMBO",
+          img: "food6.webp",
+          descricao: "Completo",
+          url: "/combo",
+          diasSemana: [
+            "segunda-feira",
+            "terça-feira",
+            "quarta-feira",
+            "quinta-feira",
+            "sexta-feira",
+            "sábado",
+            "domingo",
+          ],
+        },
+       /* {
+          id: 2,
+          nome: "PRATOS ESPECIAIS",
+          img: "food12.webp",
+          descricao: "Verificar disponibilidade",
+          url: "/macarronada",
+          diasSemana: [
+            "segunda-feira",
+            "terça-feira",
+            "quarta-feira",
+            "quinta-feira",
+            "sexta-feira",
+            "sábado",
+            "domingo",
+          ],
+        },*/
+        {
+          id: 6,
+          nome: "PORÇÕES",
+          img: "food3.webp",
+          descricao: "Disponível todos os dias",
+          url: "/batata",
+          diasSemana: [
+            "segunda-feira",
+            "terça-feira",
+            "quarta-feira",
+            "quinta-feira",
+            "sexta-feira",
+            "sábado",
+            "domingo",
+          ],
+        },
+        {
+          id: 7,
+          nome: "BURGUERES MONTADOS",
+          img: "food11.webp",
+          descricao: "Escolha seu favorito",
+          url: "/sobremesas",
+          diasSemana: [
+            "segunda-feira",
+            "terça-feira",
+            "quarta-feira",
+            "quinta-feira",
+            "sexta-feira",
+            "sábado",
+            "domingo",
+          ],
+        },
+
+        {
+          id: 4,
+          nome: "BEBIDAS",
+          img: "food4.webp",
+          descricao: "Refrescantes",
+          url: "/bebidas",
+          diasSemana: [
+            "segunda-feira",
+            "terça-feira",
+            "quarta-feira",
+            "quinta-feira",
+            "sexta-feira",
+            "sábado",
+            "domingo",
+          ],
+        },
+      ],
+    };
+  },
+});
+
+export const produtosStore2 = defineStore("produto2", {
+  state: () => {
+    return {
+      cardDataStore: [
+       /*  {
+          id: 9,
+          nome: "ASSUSTADORAMENTE",
+          img: "food9.webp",
+          descricao: "Delicioso",
+          url: "/sopas",
+          diasSemana: [
+            "segunda-feira",
+            "terça-feira",
+            "quarta-feira",
+            "quinta-feira",
+            "sexta-feira",
+            "sábado",
+            "domingo",
+          ],
+        },*/
+        {
+          id: 1,
+          nome: "BURGUER",
+          img: "food1.webp",
+          descricao: "Monte do seu jeito",
+          url: "/burger",
+          diasSemana: [
+            "segunda-feira",
+            "terça-feira",
+            "quarta-feira",
+            "quinta-feira",
+            "sexta-feira",
+            "sábado",
+            "domingo",
+          ],
+        },
+        {
+          id: 8,
+          nome: "COMBO",
+          img: "food6.webp",
+          descricao: "Completo",
+          url: "/combo",
+          diasSemana: [
+            "segunda-feira",
+            "terça-feira",
+            "quarta-feira",
+            "quinta-feira",
+            "sexta-feira",
+            "sábado",
+            "domingo",
+          ],
+        },
+       /* {
+          id: 2,
+          nome: "PRATOS ESPECIAIS",
+          img: "food12.webp",
+          descricao: "Verificar disponibilidade",
+          url: "/macarronada",
+          diasSemana: [
+            "segunda-feira",
+            "terça-feira",
+            "quarta-feira",
+            "quinta-feira",
+            "sexta-feira",
+            "sábado",
+            "domingo",
+          ],
+        },*/
+        {
+          id: 6,
+          nome: "PORÇÕES",
+          img: "food3.webp",
+          descricao: "Disponível todos os dias",
+          url: "/batata",
+          diasSemana: [
+            "segunda-feira",
+            "terça-feira",
+            "quarta-feira",
+            "quinta-feira",
+            "sexta-feira",
+            "sábado",
+            "domingo",
+          ],
+        },
+        {
+          id: 7,
+          nome: "BURGUERES MONTADOS",
+          img: "food11.webp",
+          descricao: "Escolha seu favorito",
+          url: "/sobremesas",
+          diasSemana: [
+            "segunda-feira",
+            "terça-feira",
+            "quarta-feira",
+            "quinta-feira",
+            "sexta-feira",
+            "sábado",
+            "domingo",
+          ],
+        },
+
+        {
+          id: 4,
+          nome: "BEBIDAS",
+          img: "food4.webp",
+          descricao: "Refrescantes",
+          url: "/bebidas",
+          diasSemana: [
+            "segunda-feira",
+            "terça-feira",
+            "quarta-feira",
+            "quinta-feira",
+            "sexta-feira",
+            "sábado",
+            "domingo",
+          ],
+        },
+      ],
+    };
+  },
+});
