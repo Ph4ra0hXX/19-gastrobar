62f884e5e6fca8dee93e46013e85d58edcbd0550
Ph4ra0hX_X <90359532+Ph4ra0hXX@users.noreply.github.com>
2025-04-19 10:04:15 -0300

    Update entrega.vue

--- src/pages/entrega.vue
+++ src/pages/entrega.vue
@@ -275,7 +275,7 @@ export default {
 
           carrinho.pedidos = [];
 
-          window.location.href = `https://wa.me/5588997542121?text=${this.pedidoMontado}`;
+          window.location.href = `https://wa.me/5588921635704?text=${this.pedidoMontado}`;
         } else {
           toast.warning("✏️ Preencha todos os campos", {
             timeout: 2000,
@@ -314,7 +314,7 @@ export default {
 
           carrinho.pedidos = [];
 
-          window.location.href = `https://wa.me/5588997542121?text=${this.pedidoMontado}`;
+          window.location.href = `https://wa.me/5588921635704?text=${this.pedidoMontado}`;
         } else {
           toast.warning("✏️ Preencha todos os campos", {
             timeout: 2000,
